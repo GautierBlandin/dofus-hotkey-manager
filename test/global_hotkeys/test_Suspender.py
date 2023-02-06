514574ad73a3c1ea35230637b7cb7c5830493c1b
GautierBlandin <gautier.blandin@gmail.com>
2023-02-06 13:48:16 +0100

    Add Suspender class tests

---
+++ test/global_hotkeys/test_Suspender.py
@@ -0,0 +1,22 @@
+from global_hotkeys.Suspender import Suspender
+
+
+def test_suspender():
+    def function_to_suspend():
+        return 0
+
+    suspender = Suspender()
+    assert suspender.suspended is False
+    assert suspender.make_suspendable(function_to_suspend)() == 0
+    suspender.set_suspended(True)
+    assert suspender.suspended is True
+    assert suspender.make_suspendable(function_to_suspend)() is None
+    suspender.set_suspended(False)
+    assert suspender.suspended is False
+    assert suspender.make_suspendable(function_to_suspend)() == 0
+    suspender.toggle_suspended()
+    assert suspender.suspended is True
+    assert suspender.make_suspendable(function_to_suspend)() is None
+    suspender.toggle_suspended()
+    assert suspender.suspended is False
+    assert suspender.make_suspendable(function_to_suspend)() == 0
