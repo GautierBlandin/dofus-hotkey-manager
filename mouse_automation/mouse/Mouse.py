6c84478a15b6cbd8bc6f0973f4d2140e12c1cf54
GautierBlandin <gautier.blandin@gmail.com>
2023-02-06 17:25:29 +0100

    Create Mouse class

---
+++ mouse_automation/mouse/Mouse.py
@@ -0,0 +1,15 @@
+import win32api
+import win32con
+
+
+class Mouse:
+    def click(self, x, y):
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+
+    def right_click(self, x, y):
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
