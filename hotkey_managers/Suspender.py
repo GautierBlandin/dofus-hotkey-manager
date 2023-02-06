3e79f697aca5f7066696e42be44598b412b728fb
GautierBlandin <gautier.blandin@gmail.com>
2023-02-07 00:11:52 +0100

    Add typing to Suspender

--- hotkey_managers/Suspender.py
+++ hotkey_managers/Suspender.py
@@ -1,14 +1,17 @@
+from typing import Callable
+
+
 class Suspender:
     def __init__(self, suspended=False):
         self.suspended = suspended
 
-    def toggle_suspended(self):
+    def toggle_suspended(self) -> None:
         self.suspended = not self.suspended
 
-    def set_suspended(self, suspended: bool):
+    def set_suspended(self, suspended: bool) -> None:
         self.suspended = suspended
 
-    def make_suspendable(self, function):
+    def make_suspendable(self, function) -> Callable:
         def suspended(*args, **kwargs):
             if not self.suspended:
                 return function(*args, **kwargs)
