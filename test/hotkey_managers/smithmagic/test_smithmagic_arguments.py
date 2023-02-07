c698d92d4e5035872dbe2e9b343f090b6f2422ee
GautierBlandin <gautier.blandin@gmail.com>
2023-02-07 12:13:57 +0100

    Update path configuration to allow relative path

--- test/hotkey_managers/smithmagic/test_smithmagic_arguments.py
+++ test/hotkey_managers/smithmagic/test_smithmagic_arguments.py
@@ -1,9 +1,14 @@
 from hotkey_managers.smithmagic_manager.SmithmagicArguments import SmithmagicArguments
+import os
 
 def test_smithmagic_arguments():
     """Test smithmagic_manager arguments."""
+    script_dir = os.path.dirname(__file__)
+    rel_path = './smithmagic_test_configuration.yaml'
+    abs_file_path = os.path.join(script_dir, rel_path)
+
     smithmagic_arguments = SmithmagicArguments()
-    smithmagic_arguments.load_from_yaml('smithmagic_test_configuration.yaml')
+    smithmagic_arguments.load_from_yaml(abs_file_path)
     assert smithmagic_arguments.rune_positions.normal_column_x_axis_position == 1000
     assert smithmagic_arguments.rune_positions.rune_1_y_axis_position == 100
     assert smithmagic_arguments.rune_positions.rune_13_y_axis_position == 100
